95725f2d6df5970e02ebf04dc1d56d2677fb7dc1
Camila Pinzón <camilapinz18@gmail.com>
2024-05-17 13:31:27 -0500

    useFecth start

---
+++ hooks/useFetch.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import {useState, useEffect} from 'react'
+
+const useFetch = () => {
+
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null)
+
+
+  return (
+    
+  )
+}
+
+export default useFetch
